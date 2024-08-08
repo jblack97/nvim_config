b74c498f39ea6f344f77ae037a618486d49aa9c0
jblack97 <james97black@gmail.com>
2024-08-08 18:27:29 +0100

    add descriptions to harpoon keymaps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,40 +1,40 @@
 return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local harpoon = require 'harpoon'
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
 
-    -- REQUIRED
-    harpoon:setup()
-    -- REQUIRED
+		-- REQUIRED
+		harpoon:setup()
+		-- REQUIRED
 
-    vim.keymap.set('n', '<leader>ha', function()
-      harpoon:list():append()
-    end)
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():append()
+		end, { desc = "Harpoon the current file" })
 
-    vim.keymap.set('n', '<leader>hl', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
-    vim.keymap.set('n', '<leader>h1', function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set('n', '<leader>h2', function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set('n', '<leader>h3', function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set('n', '<leader>h4', function()
-      harpoon:list():select(4)
-    end)
+		vim.keymap.set("n", "<leader>hl", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "List harpooned files" })
+		vim.keymap.set("n", "<leader>h1", function()
+			harpoon:list():select(1)
+		end, { desc = "Jump to harpooned file 1" })
+		vim.keymap.set("n", "<leader>h2", function()
+			harpoon:list():select(2)
+		end, { desc = "Jump to harpooned file 2" })
+		vim.keymap.set("n", "<leader>h3", function()
+			harpoon:list():select(3)
+		end, { desc = "Jump to harpooned file 3" })
+		vim.keymap.set("n", "<leader>h4", function()
+			harpoon:list():select(4)
+		end, { desc = "Jump to harpooned file 4" })
 
-    -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '<leader>hp', function()
-      harpoon:list():prev()
-    end)
-    vim.keymap.set('n', '<leader>hn', function()
-      harpoon:list():next()
-    end)
-  end,
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<leader>hp", function()
+			harpoon:list():prev()
+		end, { desc = "Toggle previous buffer in Harpoon list" })
+		vim.keymap.set("n", "<leader>hn", function()
+			harpoon:list():next()
+		end, { desc = "Toggle next buffer in Harpoon list" })
+	end,
 }
--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,10 +1,14 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('oil').setup {
-      columns = { 'icon' },
-    }
-  end,
+	"stevearc/oil.nvim",
+	opts = {},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			columns = { "icon" },
+			keymaps = {
+				["<C-p>"] = false,
+				["<C-i>"] = "actions.preview",
+			},
+		})
+	end,
 }
